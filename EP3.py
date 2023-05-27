2edfb5dd4a1bfd24ed0211ec673bd2da6e7bd5e7
ChayadaMameaw <st15774@vru.ac.th>
2023-05-27 16:16:11 +0700

    SaveEp3

---
+++ EP3.py
@@ -0,0 +1,10 @@
+#string => ข้อความ ตัวอักษร 
+#number => integer , float
+#boolean => true , false
+print(-1)
+print(0)
+print("1"+"9")
+print(3.66)
+print(True)
+print(False)
+print("Chayada \n" + "studio")
