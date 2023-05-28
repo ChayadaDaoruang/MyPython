2ffc812f9e07b100eabaf76bedbd2a9dec0ae5da
ChayadaMameaw <st15774@vru.ac.th>
2023-05-28 15:13:24 +0700

    EP4.py

---
+++ EP4.py
@@ -0,0 +1,12 @@
+
+# ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+
+x = 20 #integer
+y = 3.88
+z = True
+# print("ผลลัพธ์ = "+str(x)) 
+# print(y)
+# print(z)
+print(type(y))
+print(type("mameaw"))
+print(type(z))
