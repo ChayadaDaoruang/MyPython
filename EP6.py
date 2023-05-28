aef345529f458a868b680deade219bf1c96bd757
ChayadaMameaw <st15774@vru.ac.th>
2023-05-28 16:35:49 +0700

    EP6.py

---
+++ EP6.py
@@ -0,0 +1,19 @@
+#Type Conversion
+x = 10
+y = 3.87
+z = "20"
+
+# "20" = > 20
+# 10 => "10"
+# บวกเลข
+z = float(z)
+ # 10+3.14 = 13.14
+ #"10" + "20" = "1020"  => result
+
+z=z+50
+# string => int
+# int => string
+
+#int => float
+# float => int
+print(type(z))
