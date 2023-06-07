4b2b61f4b0528105263fa4adcb1781e438373429
ChayadaMameaw <st15774@vru.ac.th>
2023-06-07 20:37:52 +0700

    IF

---
+++ 16.py
@@ -0,0 +1,17 @@
+age=int(input("15 : "))
+
+if age<=18 :
+    if age ==18 :
+        print("ม.6")
+    elif age==16:
+        print("ม.4")
+    elif age==14:
+        print("ม.2")
+    elif age==12 :
+        print("ป.6")
+    else :
+        print("ม.ปลาย")
+else:
+    print("มหาลัย")
+
+print("จบโปรแกรม")
