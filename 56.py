ba0e14c93e9692697bcd2835a1c227be5c5a3ae3
ChayadaMameaw <st15774@vru.ac.th>
2023-06-21 21:43:13 +0700

    56.py

---
+++ 56.py
@@ -0,0 +1,16 @@
+p,m,a=20,30,40
+
+if p%6 == 0:
+    print("คู่")
+else :
+    print("คี่")
+
+if m%7 == 0:
+     print("คู่")
+else :
+    print("คี่")
+
+if a%8 == 0:
+    print("คู่")
+else :
+    print("คี่")
