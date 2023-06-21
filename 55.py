eb4b6e237276b72cbb16a177cf12e9f623ee9312
ChayadaMameaw <st15774@vru.ac.th>
2023-06-21 21:32:36 +0700

    Ep55

---
+++ 55.py
@@ -0,0 +1,9 @@
+lis=["green","blue","baby pink"]
+tup=["brown","black","white"]
+
+colors=dict({"purple":"ม่วง","red":"แดง"})
+colors.update({"yellow":"เหลือง","orange":"ส้ม"})
+
+print(colors)
+del colors
+print(colors)
