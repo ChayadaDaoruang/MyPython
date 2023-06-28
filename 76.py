29fa7659a19458f6f1b6fbdc001437a5ccb0e8b0
ChayadaMameaw <st15774@vru.ac.th>
2023-06-28 21:28:25 +0700

    28/06/2023

---
+++ 76.py
@@ -0,0 +1,9 @@
+
+
+try:
+    number1=int(input("ป้อนตัวเลข 1 :"))
+    number2=int(input("ป้อนตัวเลข 2 :"))
+    result=number1/number2
+    print(result)
+except:
+    print("โปรแกรมผิดพลาด")
---
+++ 77.py
@@ -0,0 +1,13 @@
+
+try:
+    score=100+"60"
+    print(score)
+
+except ValueError:
+    pass
+except ZeroDivisionError:
+    pass
+except TypeError:
+    pass
+
+
